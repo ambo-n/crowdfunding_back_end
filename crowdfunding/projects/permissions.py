0b8adf4f36e44af3e18e8bede8294da31bba99fb
Amber <amber.quynh.nguyen@gmail.com>
2024-10-13 14:18:09 +0800

    permission for pledge and project done

--- crowdfunding/projects/permissions.py
+++ crowdfunding/projects/permissions.py
@@ -1,6 +1,25 @@
 from rest_framework import permissions
+from users .models import CustomUser
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self,request,view,obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner ==request.user
+
+class IsAdminorLimitView(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            if request.user.is_staff:
+                return True
+            else:
+                return False
+        else:
+            return True
+        
+class IsSupporterOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            if request.user == obj.support or request.user.is_staff:
+                return True
+        return obj.support == request.user
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.http import Http404
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
-from .permissions import IsOwnerOrReadOnly
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer
+from .permissions import IsOwnerOrReadOnly, IsAdminorLimitView, IsSupporterOrReadOnly
 
 class ProjectList(APIView):
     permission_classes =[permissions.IsAuthenticatedOrReadOnly]
@@ -69,7 +69,8 @@ class ProjectDetail(APIView):
         )
     
 class PledgeList(APIView):
-    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
+    permission_classes =[IsAdminorLimitView,
+                         permissions.IsAuthenticatedOrReadOnly]
     
     def get(self,request):
         pledges = Pledge.objects.all()
@@ -82,4 +83,35 @@ class PledgeList(APIView):
             serializer.save(support=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PledgeDetail(APIView):
+    permission_classes = [IsSupporterOrReadOnly]
+    def get_object(self,pk):
+        try:
+            pledge = Pledge.objects.get(pk=pk)
+            self.check_object_permissions(self.request,pledge)
+            return pledge
+        except Pledge.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeDetailSerializer(pledge)
+        return Response(serializer.data)
     
+    def put(self,request,pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeDetailSerializer(
+            instance = pledge,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+        
