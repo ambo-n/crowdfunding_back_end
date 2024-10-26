56d9672711b378ceab6d1b286bd69cf505c0870d
Amber <amber.quynh.nguyen@gmail.com>
2024-10-26 11:33:08 +0800

    sessionAuth under settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -53,7 +53,9 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',   
     ]
 }
 
