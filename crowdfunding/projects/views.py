d1f37e3b70105fbea33d20c9303613f39fcfd3f3
Amber <amber.quynh.nguyen@gmail.com>
2024-12-01 18:28:31 +0800

    remove category

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.http import Http404
-from .models import Project, Pledge, Category
-from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer, CategorySerializer, CategoryDetailSerializer
+from .models import Project, Pledge
+# from .models import Category
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer
+# from .serializers import CategorySerializer, CategoryDetailSerializer
 from .permissions import IsOwnerOrReadOnly, IsAdminorLimitView, IsSupporterOrReadOnly, IsAdminorViewOnly
 
 class ProjectList(APIView):
@@ -32,54 +34,54 @@ class ProjectList(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-class CategoryList(APIView):
-    def get(self,request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-    def post(self,request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-class CategoryDetail(APIView):
-    permission_classes = [IsAdminorViewOnly]
-    def get_object(self,pk):
-        try:
-            category = Category.objects.get(pk=pk)
-            return category
-        except Category.DoesNotExist:
-            return Http404
-    def get(self, request, pk):
-        category = self.get_object(pk)
-        serializer = CategoryDetailSerializer(category)
-        return Response(serializer.data)
+# class CategoryList(APIView):
+#     def get(self,request):
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True)
+#         return Response(serializer.data)
+#     def post(self,request):
+#         serializer = CategorySerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(
+#                 serializer.data,
+#                 status=status.HTTP_201_CREATED
+#             )
+#         return Response(
+#             serializer.errors,
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
+# class CategoryDetail(APIView):
+#     permission_classes = [IsAdminorViewOnly]
+#     def get_object(self,pk):
+#         try:
+#             category = Category.objects.get(pk=pk)
+#             return category
+#         except Category.DoesNotExist:
+#             return Http404
+#     def get(self, request, pk):
+#         category = self.get_object(pk)
+#         serializer = CategoryDetailSerializer(category)
+#         return Response(serializer.data)
     
-    def put(self, request,pk):
-        category = self.get_object(pk=pk)
-        serializer = CategoryDetailSerializer(
-            instance=category,
-            data = request.data,
-            partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status = status.HTTP_400_BAD_REQUEST
-        )
-    def delete(self,request,pk):
-        category = self.get_object(pk)
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def put(self, request,pk):
+#         category = self.get_object(pk=pk)
+#         serializer = CategoryDetailSerializer(
+#             instance=category,
+#             data = request.data,
+#             partial=True
+#         )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(
+#             serializer.errors,
+#             status = status.HTTP_400_BAD_REQUEST
+#         )
+#     def delete(self,request,pk):
+#         category = self.get_object(pk)
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 class ProjectDetail(APIView):
 
     permission_classes =[
