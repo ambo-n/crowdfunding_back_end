ca28114130f5918ad39bee5622ded24a0cd11144
Amber <amber.quynh.nguyen@gmail.com>
2025-04-04 21:38:16 +0800

    wrote tests for project and pledge serializers

--- crowdfunding/projects/tests.py
+++ crowdfunding/projects/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from projects.models import Project, Category, Pledge
-from projects.serializers import ProjectSerializer
+from projects.serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -73,7 +73,21 @@ class ProjectSerializerTest(TestCase):
         }
         serializer = ProjectSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
-
+    
+    def test_project_update_serializer(self):
+        project = Project.objects.get(pk=self.project.id)
+        project.title="Test Project Update"
+        project.description="An updated descriptor"
+        project.goal=80
+        project.is_open=False
+        project.save()
+        
+        serializer = ProjectDetailSerializer(project)
+        data = serializer.data
+        self.assertEqual(data["title"],"Test Project Update")
+        self.assertEqual(data["description"], "An updated descriptor")
+        self.assertFalse(data["is_open"])
+    
 class PledgeModelTest(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(username="testuser", password="password123")
@@ -104,6 +118,86 @@ class PledgeModelTest(TestCase):
         self.assertEqual(pledge.comment, "test pledge")
         self.assertEqual(pledge.project, self.project)
         self.assertEqual(pledge.support, self.user)
+
+class PledgeSerializerTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="testuser", password="password123")
+        self.donor = get_user_model().objects.create_user(username="pledgemaker", password="pledger123")
+        self.category = Category.objects.create(description= "Test_category")
+        self.project = Project.objects.create(
+            title="Test Project",
+            description="A sample project",
+            goal=5000,
+            image="https://example.com/image.jpg",
+            is_open=True,
+            address= "37 Halsey Road",
+	        suburb= "Tunkalilla",
+	        postcode= 5203,
+	        state="SA",
+            owner=self.user,
+        )
+        self.project.category.add(self.category)
+        self.pledge = Pledge.objects.create(
+            amount = 5000,
+            comment = "test pledge",
+            anonymous = False,
+            project = self.project,
+            support = self.donor
+        )
+        self.pledge2 = Pledge.objects.create(
+            amount = 5000,
+            comment = "test pledge2",
+            anonymous = False,
+            project = self.project,
+            support = self.donor
+        )
+    
+    def test_pledge_serializer(self):
+        serializer = PledgeSerializer(self.pledge)
+        data = serializer.data
+        self.assertEqual(data["amount"], 5000)
+        self.assertEqual(data["comment"], "test pledge")
+        self.assertEqual(data["support"], self.donor.id)
+    
+    def test_pledge_deserialization(self):
+        data = {
+            "amount":1000,
+            "comment":"Keep up the good work",
+            "anonymous":True,
+            "project":self.project.id,
+            "support":self.donor
+        }
+        serializer= PledgeSerializer(data=data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        invalid_data = {
+            "amount":1000,
+            "project":self.project,
+            "support":self.donor
+            
+        }
+        serializer = PledgeSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid(), serializer.errors)
+    
+    def test_pledge_update(self):
+        pledge = Pledge.objects.get(pk=self.pledge2.id)
+        pledge.amount = 800
+        pledge.comment = "Keep up the good work updated"
+        pledge.anonymous = False
+        pledge.save()
+        serializer = PledgeDetailSerializer(pledge)
+        data = serializer.data
+        self.assertEqual(data["amount"], 800)
+        self.assertEqual(data["comment"], "Keep up the good work updated")
+        self.assertFalse(data["anonymous"])
+    
+
+    def test_project_detail_serializer(self):
+        serializer = ProjectDetailSerializer(self.project)
+        data = serializer.data
+        self.assertEqual(data["pledges"][0]["amount"], 5000)
+        self.assertEqual(len(data["pledges"]), 2)
+
+
 
 class ProjectAPITest(TestCase):
     def setUp(self):
