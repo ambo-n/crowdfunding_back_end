7c84857529eecf673ad71272e730b89d86939049
Amber <amber.quynh.nguyen@gmail.com>
2025-04-16 13:01:44 +0800

    changes to user model and wrote test for user model

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,6 +1,35 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set.")
+        if not email:
+            raise ValueError("The Email must be set.")
+        if not password:
+            raise ValueError("The Password must be set.")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-
+    email = models.EmailField(max_length=255, blank=False, unique=True)
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+    objects = CustomUserManager()
     def __str__(self):
         return self.username
